0cac83c55dbbe682310b9a3b44c470cdf68f1fc4
malkoas <41993717+malkoas@users.noreply.github.com>
2021-09-08 09:20:38 +0300

    refactor converter

--- src/main/java/com/rbkmoney/trusted/tokens/converter/RowConverter.java
+++ src/main/java/com/rbkmoney/trusted/tokens/converter/RowConverter.java
@@ -2,8 +2,6 @@ package com.rbkmoney.trusted.tokens.converter;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.rbkmoney.trusted.tokens.ConditionTemplate;
-import com.rbkmoney.trusted.tokens.model.CardTokenData;
 import com.rbkmoney.trusted.tokens.model.Row;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -15,25 +13,15 @@ public class RowConverter {
 
     private final ObjectMapper objectMapper;
 
-    public Row convert(String conditionTemplateName, ConditionTemplate conditionTemplate) {
+    public Row convert(String key, Object value) {
         try {
             Row row = new Row();
-            row.setKey(conditionTemplateName);
-            row.setValue(objectMapper.writeValueAsString(conditionTemplate));
+            row.setKey(key);
+            row.setValue(objectMapper.writeValueAsString(value));
             return row;
         } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
         }
     }
 
-    public Row convert(String cardToken, CardTokenData cardTokenData) {
-        try {
-            Row row = new Row();
-            row.setKey(cardToken);
-            row.setValue(objectMapper.writeValueAsString(cardTokenData));
-            return row;
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
-    }
 }
